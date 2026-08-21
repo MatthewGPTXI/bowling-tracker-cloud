4ba44f807d9e5ce8b2347bd6f060b4d168357394
MatthewGPTXI <mtwilsonxi@gmail.com>
2026-08-21 11:36:45 -0700

    Fix Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // Bowling Tracker Firebase connection.
 // Firebase web API keys are public identifiers; Firestore Security Rules protect the data.
 window.BOWLING_FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyAtAD3wBoWSbBSxylF4BU2x5DFK9joHLC8',
+  apiKey: 'AIzaSyAtAD3wBOWSbBSxylF4BU2x5DFK9joHlC8',
   authDomain: 'bowling-tracker-aad74.firebaseapp.com',
   projectId: 'bowling-tracker-aad74',
   storageBucket: 'bowling-tracker-aad74.firebasestorage.app',
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bowling-tracker-v2-cloud';
+const CACHE_NAME = 'bowling-tracker-v2-cloud-r1';
 const APP_ASSETS = [
   './',
   './index.html',
